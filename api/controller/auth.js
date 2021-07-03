008f6af34ce29633c830884bde53cf8963da7896
jasonczx <jasonczx@endstruct.com>
2021-07-03 09:48:12 +0800

    windows: move code up [web]

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -4,22 +4,20 @@ const db = require("../model/general/sql")
 const dbT = "customers"
 
 
-const loginUser = async (data, auth) => {
+const loginUser = async(data) => {
     let reqResult = new requestVM()
     try {
         let query = `Select COUNT(*) as COUNT from ${dbT} where userid=? and password=? and status=1`
         query = db.mysql.format(query, [data.userid, data.password]);
         let result = await database.Get(query, true)
         return result
-    }
-    catch (e) {
+    } catch (e) {
         return reqResult.req_Fail("api/user/loginUser", e)
     }
 }
 
 
 
-module.exports = function () {
+module.exports = function() {
     loginUser
 }
-
